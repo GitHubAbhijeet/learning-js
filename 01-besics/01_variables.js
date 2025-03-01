971580ec53a6575c0ce55bc2169e4db77993d244
Abhijeet Dhawale <153369585+GitHubAbhijeet@users.noreply.github.com>
2025-03-01 14:14:07 +0000

    added js becics code

---
+++ 01-besics/01_variables.js
@@ -0,0 +1,8 @@
+const accountID = 144553
+let accountEmail = "dfgh@google.com"
+var accountPassword = "12e543"
+accountCity = "Pune"
+
+//accountID = 2 //not allowed
+
+console.log(accountPassword);
